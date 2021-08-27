0a02ff1128914cb2994ca6b88d797431750f53c4
Terryxyz <36756432+Terryxyz@users.noreply.github.com>
2021-08-27 10:42:26 +0800

    Update and rename label_software.py to label_Tier1.py

--- annotating_software/label_Tier1.py
+++ annotating_software/label_Tier1.py
@@ -103,14 +103,10 @@ def draw_shape(event, x, y, flags, param):
 
 def main():
     global mode_index, color_index, drawing, showing_finger_line, ix, iy, rx, ry, label_vis, color_heightmap_bgr, depth_heightmap_bgr, label_array_color, label_array
-    label_figure_path = "/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao"
-    #label_figure_path = input("请输入图片数据集的地址")
-    #input_data_index = int(input("请输入开始标定的图片的序号"))
-    input_data_index_index = 0
-    input_data_index_set = [6021, 6089, 6108, 6113, 6121, 6175, 6212, 6219, 6220, 6242, 6272, 6281, 6299, 6328, 6343, 6351, 6381, 6400, 6407, 6469, 6573, 6582, 6583, 6652, 6653, 6745, 6807, 6808, 6817, 6843, 6851, 6868, 6897, 6905, 6917, 6947, 6979, 6980]
+    label_figure_path = input("Please enter the address of the picture dataset")
+    input_data_index = int(input("Please enter the index of the picture to start calibration"))
     while True:
-        input_data_index = input_data_index_set[input_data_index_index]
-        print('图片序号', input_data_index)
+        print('image_index', input_data_index)
         color_heightmap_array = cv2.imread(label_figure_path + "/figure/rgb_image_"+str(input_data_index)+".png")[:, :, [2,1,0]]
         depth_heightmap_array = cv2.imread(label_figure_path + "/figure/depth_image_"+str(input_data_index)+".png", -1)
         label_array = cv2.imread(label_figure_path + "/figure/label_initial_"+str(input_data_index)+".png", -1).astype(np.float32)
@@ -200,123 +196,13 @@ def main():
         if control_signal == "exit":
             break
         elif control_signal == "next":
-            input_data_index_index += 1
+            input_data_index += 1
             continue
         elif control_signal == "last": 
-            input_data_index_index -= 1 
+            input_data_index -= 1 
             continue     
 
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    #main()
-    '''
-    for i in  range(21, 1000):
-        print(i)
-        left_fig = cv2.imread("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/figure_Acrylic/rgb_image_"+str(i)+".png")[:, :, [2,1,0]]
-        plt.subplot(1, 2, 1)
-        plt.imshow(left_fig)
-        right_fig = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_Acrylic/label_"+str(i)+".npy")
-        plt.subplot(1, 2, 2)
-        plt.imshow(right_fig, cmap='gray')
-        plt.rcParams["keymap.quit"] = 'enter'
-        plt.show()
-    '''
-    for index in range(1, 12001):
-        print(index)
-        current_array = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_Acrylic/label_"+str(index)+".npy").astype(np.uint8)
-        if 'label_data_array_Acrylic_taobao' not in dir():
-            label_data_array_Acrylic_taobao = current_array[np.newaxis, :, :]
-        else:
-            label_data_array_Acrylic_taobao = np.concatenate((label_data_array_Acrylic_taobao, current_array[np.newaxis, :, :]), axis = 0)
-    print(label_data_array_Acrylic_taobao.shape)
-    np.save("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_data_array_Acrylic_taobao.npy", label_data_array_Acrylic_taobao)
-
-    '''
-    for index in range(0, 1500):
-        print(index)
-        current_array = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_Go_stone/Go_stone_label_"+str(index)+".npy").astype(np.uint8)
-        if 'label_data_array_Go_stone_taobao' not in dir():
-            label_data_array_Go_stone_taobao = current_array[np.newaxis, :, :]
-        else:
-            label_data_array_Go_stone_taobao = np.concatenate((label_data_array_Go_stone_taobao, current_array[np.newaxis, :, :]), axis = 0)
-    print(label_data_array_Go_stone_taobao.shape)
-    np.save("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_data_array_Go_stone_taobao.npy", label_data_array_Go_stone_taobao)
-    '''
-
-
-    label_1 = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_data_array_domino_taobao.npy")
-    print('1')
-    label_2 = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_data_array_Acrylic_taobao.npy")
-    print('2')
-    label_3 = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_data_array_Go_stone_taobao.npy")
-    print('3')
-    label_20210715 = np.concatenate((np.concatenate((label_1, label_2), axis = 0), label_3), axis = 0)
-    print(label_20210715.shape)
-    np.save("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_data_array_20210715.npy", label_20210715)
-
-    '''
-    concatenate_list_domino = []
-    for i in range(1, 8625):
-        print('domino', i)
-        current_figure_rgb = cv2.imread("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/figure_domino/rgb_image_"+str(i)+".png")[:, :, [2,1,0]][np.newaxis, :, :, :].astype(np.uint8)
-        current_figure_depth = cv2.imread("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/figure_domino/depth_image_"+str(i)+".png", -1)[:, :, np.newaxis][np.newaxis, :, :, :].astype(np.uint8)
-        current_figure_rgbd = np.concatenate((current_figure_rgb, current_figure_depth), axis = 3)
-        concatenate_list_domino.append(current_figure_rgbd)
-    input_data_domino_rgb = np.concatenate(concatenate_list_domino, axis = 0)
-    print(input_data_domino_rgb.shape)
-    np.save("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/input_data_array_domino_20210703.npy", input_data_domino_rgb)
-    '''
-    '''
-    concatenate_list_Acrylic = []
-    for i in range(1, 12001):
-        print('Acrylic', i)
-        current_figure_rgb = cv2.imread("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/figure_Acrylic/rgb_image_"+str(i)+".png")[:, :, [2,1,0]][np.newaxis, :, :, :].astype(np.uint8)
-        current_figure_depth = cv2.imread("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/figure_Acrylic/depth_image_"+str(i)+".png", -1)[:, :, np.newaxis][np.newaxis, :, :, :].astype(np.uint8)
-        current_figure_rgbd = np.concatenate((current_figure_rgb, current_figure_depth), axis = 3)
-        concatenate_list_Acrylic.append(current_figure_rgbd)
-    input_data_Acrylic_rgb = np.concatenate(concatenate_list_Acrylic, axis = 0)
-    print(input_data_Acrylic_rgb.shape)
-    np.save("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/input_data_array_Acrylic_20210715.npy", input_data_Acrylic_rgb)
-    '''
-    '''
-    concatenate_list_Go_stone = []
-    for i in range(0, 1500):
-        print('Go_stone', i)
-        current_figure_rgb = cv2.imread("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/figure_Go_stone/rgb_image_"+str(i)+".png")[:, :, [2,1,0]][np.newaxis, :, :, :].astype(np.uint8)
-        current_figure_depth = cv2.imread("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/figure_Go_stone/depth_image_"+str(i)+".png", -1)[:, :, np.newaxis][np.newaxis, :, :, :].astype(np.uint8)
-        current_figure_rgbd = np.concatenate((current_figure_rgb, current_figure_depth), axis = 3)
-        concatenate_list_Go_stone.append(current_figure_rgbd)
-    input_data_Go_stone_rgb = np.concatenate(concatenate_list_Go_stone, axis = 0)
-    print(input_data_Go_stone_rgb.shape)
-    np.save("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/input_data_array_Go_stone_20210709.npy", input_data_Go_stone_rgb)
-    '''
-    '''
-    input_domino = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/input_data_array_domino_20210703.npy") 
-    print('1')
-    input_Acrylic = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/input_data_array_Acrylic_20210715.npy")
-    print('2')
-    input_Go_stone = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/input_data_array_Go_stone_20210709.npy")
-    print('3')
-    input_data = np.concatenate((input_domino, input_Acrylic, input_Go_stone), axis=0)
-    print(input_data.shape)
-    np.save("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/input_data_array_20210715.npy", input_data)
-    '''
-    
-    
-    '''
-    input_data = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/input_data_array_20210703.npy")
-    label_data = np.load("/home/terry/catkin_ws/src/dg_learning_real_one_net/label_taobao/label_data_array_20210703.npy")
-    for i in  range(9000, 10000):
-        print(i)
-        left_fig = input_data[i, :, :, 0:3]
-        plt.subplot(1, 2, 1)
-        plt.imshow(left_fig)
-        right_fig = label_data[i, :, :]
-        plt.subplot(1, 2, 2)
-        plt.imshow(right_fig, cmap='gray')
-        plt.rcParams["keymap.quit"] = 'enter'
-        plt.show()
-    '''
-    
-    
+    main()
